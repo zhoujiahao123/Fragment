bf557645f08064d9b40046c6db98ea55b8302a7d
zhoujiahao <zhou.jiahao@foxmail.com>
2016-12-17 19:26:59 +0800

    Test the pop

--- app/src/main/java/com/nexulink/jacob/bean/ui/activity/BookDetailActivity.java
+++ app/src/main/java/com/nexulink/jacob/bean/ui/activity/BookDetailActivity.java
@@ -1,9 +1,14 @@
 package com.nexulink.jacob.bean.ui.activity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
 
 import com.nexuslink.www.googlefragment.R;
 
@@ -11,24 +16,32 @@ import com.nexuslink.www.googlefragment.R;
  * Created by ASUS-NB on 2016/12/14.
  */
 
-public class BookDetailActivity extends AppCompatActivity {
+public class BookDetailActivity extends AppCompatActivity implements View.OnClickListener {
+    BookDetailsFragment fragment;
     @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_bookdetail_layout);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         if(savedInstanceState==null){
-            BookDetailsFragment fragment =new BookDetailsFragment();
+             fragment=new BookDetailsFragment();
             Bundle arguments = new Bundle();
             arguments.putInt(BookDetailsFragment.ITEM_ID,getIntent().getIntExtra(BookDetailsFragment.ITEM_ID,0));
             fragment.setArguments(arguments);
             getFragmentManager().beginTransaction()
                     .add(R.id.book_container,fragment)
+                    .addToBackStack(null)
                     .commit();
         }
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.goback);
+        toolbar.setNavigationOnClickListener(this);
+
+}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,5 +49,10 @@ public class BookDetailActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/nexulink/jacob/bean/ui/activity/BookDetailsFragment.java
+++ app/src/main/java/com/nexulink/jacob/bean/ui/activity/BookDetailsFragment.java
@@ -4,6 +4,7 @@ package com.nexulink.jacob.bean.ui.activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
